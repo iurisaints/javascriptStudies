35366dc99dd3e50c861fb4ddbd74d39c98570738
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-11 11:08:01 -0300

    Create 1004.js

---
+++ Beecrowd/1004.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let a = Number(lines.shift())
+let b = Number(lines.shift())
+let prod = a * b
+console.log(`PROD = ${prod}`)
